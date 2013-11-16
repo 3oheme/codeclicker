e0f2868434602226bb05f3a58b2262cb24cc8d56
3oheme <ignacio.palomo.duarte@gmail.com>
2013-11-16 20:11:34 +0000

    @3oheme testing adding graduates to help with source code

--- codeclicker.js
+++ codeclicker.js
@@ -1,8 +1,15 @@
 angular.module('CodeClicker', [])
   .controller('GameController', function() {
   	this.lines_of_code = 0;
-
+  	this.graduates = 0;
     this.codeclick = function() {
     	this.lines_of_code += 1;
     }
+    this.buy = function(item) {
+    	if (item = 'graduates' && this.lines_of_code > 10) {
+    		this.graduates += 1;
+    		this.lines_of_code -= 10;
+    	}
+    }
+
   });
