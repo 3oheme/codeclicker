93fd8367e10626d6794179f964cbb58507075c07
3oheme <ignacio.palomo.duarte@gmail.com>
2013-11-16 20:43:13 +0000

    @3oheme a non AngularJS proper solution :-(

--- codeclicker.js
+++ codeclicker.js
@@ -2,14 +2,22 @@ angular.module('CodeClicker', [])
   .controller('GameController', function() {
   	this.lines_of_code = 0;
   	this.graduates = 0;
-    this.codeclick = function() {
+  	this.graduates_clock;
+    
+    this.codeclick = function codeclick(){
     	this.lines_of_code += 1;
-    }
+    };
+    
     this.buy = function(item) {
     	if (item = 'graduates' && this.lines_of_code > 10) {
     		this.graduates += 1;
     		this.lines_of_code -= 10;
+    		clearInterval(this.graduates_clock);
+    		this.graduates_clock = setInterval(function(){
+    			document.getElementById('console').click();
+    			console.log('hey')},
+    			1000/this.graduates);
     	}
-    }
+    };
 
   });
