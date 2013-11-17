a23e6285dfdfceb6769f3a94ad50e44123480dd9
3oheme <ignacio.palomo.duarte@gmail.com>
2013-11-17 01:22:08 +0000

    @3oheme added power ups collection

--- codeclicker.js
+++ codeclicker.js
@@ -1,23 +1,51 @@
-angular.module('CodeClicker', [])
-  .controller('GameController', function() {
-  	this.lines_of_code = 0;
-  	this.graduates = 0;
-  	this.graduates_clock;
+var game = angular.module('CodeClicker', []);
+
+game.controller('GameController', ['$scope', function($scope, gameStatus) {
+  	$scope.lines_of_code = 0;
+  	$scope.graduates = 0;
+  	$scope.graduates_clock;
     
-    this.codeclick = function codeclick(){
-    	this.lines_of_code += 1;
+    $scope.codeclick = function codeclick(){
+    	$scope.lines_of_code += 1;
     };
     
-    this.buy = function(item) {
-    	if (item = 'graduates' && this.lines_of_code > 10) {
-    		this.graduates += 1;
-    		this.lines_of_code -= 10;
-    		clearInterval(this.graduates_clock);
-    		this.graduates_clock = setInterval(function(){
-    			document.getElementById('console').click();
-    			console.log('hey')},
-    			1000/this.graduates);
+    $scope.buy = function(item) {
+    	if (item = 'graduates' && $scope.lines_of_code > 10) {
+    		$scope.graduates += 1;
+    		$scope.lines_of_code -= 10;
+    		clearInterval($scope.graduates_clock);
+    		$scope.graduates_clock = setInterval(function(){
+    			document.getElementById('console').click();},
+    			1000/$scope.graduates);
     	}
     };
 
-  });
+}]);
+
+game.controller('PowerUpsController', ['$scope', function($scope) {
+
+	$scope.powerups = [
+	    {
+	   		'id': 'graduates',
+	   		'name': 'Graduates',
+	   		'price': 10,
+	   		'lps': 1,
+	   		'count': 0,
+	    },
+	    {
+	   		'id': 'consultants',
+	   		'name': 'Consultants',
+	   		'price': 50,
+	   		'lps': 2,
+	   		'count': 0,
+	    },
+	    {
+	   		'id': 'senior',
+	   		'name': 'Senior',
+	   		'price': 200,
+	   		'lps': 5,
+	   		'count': 0,
+	    }
+	];
+
+}]);
